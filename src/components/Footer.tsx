eefbdbc3e713aced32e91cbb5b2f5338e0007632
karifol <karigolhorinouchi@gmail.com>
2024-02-12 10:10:54 +0900

    dev

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,44 @@
 import { View, StyleSheet } from 'react-native'
 import FooterButton from './FooterButton'
 import { AntDesign, FontAwesome, FontAwesome6 } from '@expo/vector-icons'
+import { useRoute } from '@react-navigation/native'
 
 const Footer = (): JSX.Element => {
+  const route = useRoute()
+  const selected = route.name.split('/')[1] // 現在のページ名
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
-        <FooterButton labelText='メニュー' render='/amedas/menue' icon={<AntDesign name="bars" size={40}/>}/>
-        <FooterButton labelText='マップ' render='/amedas/map' icon={<FontAwesome name="map" size={40}/>} />
-        <FooterButton labelText='ランキング' render='/amedas/ranking' icon={<FontAwesome6 name="ranking-star" size={40}/>} />
-        <FooterButton labelText='グラフ' render='/amedas/graph' icon={<AntDesign name="linechart" size={40}/>}/>
+        <FooterButton
+          labelText='メニュー'
+          render='/amedas/menue'
+          icon={<AntDesign name="bars" size={40} style={
+            selected === 'menue' ? styles.iconSelected : styles.icon
+          }/>}
+        />
+        <FooterButton
+          labelText='マップ'
+          render='/amedas/map'
+          icon={<FontAwesome name="map"
+          size={40}
+          style={
+            selected === 'map' ? styles.iconSelected : styles.icon
+          }/>}
+        />
+        <FooterButton
+          labelText='ランキング'
+          render='/amedas/ranking'
+          icon={<FontAwesome6 name="ranking-star" size={40} style={
+            selected === 'ranking' ? styles.iconSelected : styles.icon
+          }/>}
+        />
+        <FooterButton
+          labelText='グラフ'
+          render='/amedas/graph'
+          icon={<AntDesign name="linechart" size={40} style={
+            selected === 'graph' ? styles.iconSelected : styles.icon
+          }/>}
+        />
       </View>
     </View>
   )
@@ -27,6 +56,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     width: '100%'
+  },
+  icon: {
+    color: 'rgb(161, 161, 161)'
+  },
+  iconSelected: {
+    color: 'rgb(44, 161, 185)'
   }
 })
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
 import { View, Text, StyleSheet } from 'react-native'
+import { useRoute } from '@react-navigation/native'
 
 const Header = (): JSX.Element => {
+  const fileObj = {
+    menue: 'メニュー',
+    map: 'マップ',
+    ranking: 'ランキング',
+    graph: 'グラフ'
+  } as const
+  const route = useRoute()
+  const selected = route.name.split('/')[1] as keyof typeof fileObj
+  const title = fileObj[selected] as string
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>HeaderText</Text>
+      <Text style={styles.title}>{title}</Text>
     </View>
   )
 }
